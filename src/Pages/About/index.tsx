a5bacbe3eb5d9c7186f74a3c08eb33238eeba09a
Pedro Chaves <pedro.chaves@cittamobi.com.br>
2021-09-10 22:16:37 -0300

    Base da página de contato

--- src/Pages/About/index.tsx
+++ src/Pages/About/index.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "./about.scss";
+import aboutImg from "../../assets/img/aboutImg.svg";
+import waveBG from "../../assets/img/wave.svg";
 
 const About = () => {
   return (
-    <div>
-      <p>About</p>
+    <div id="about">
+      <img id="imagem" src={aboutImg} alt="yes" />
+      <div id="description">
+        <img id="background-wave" src={waveBG} alt="" />
+        <h2>Contato</h2>
+        <ul>Telefone</ul>
+      </div>
     </div>
   );
 };
