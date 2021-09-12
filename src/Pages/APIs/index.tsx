0a2b5e3fa3ba5d0ac237e54cb7ed7403f23f85ac
Pedro Chaves <pedro.chaves@cittamobi.com.br>
2021-09-12 17:05:51 -0300

    Primeira versão da página da APIS

--- src/Pages/APIs/index.tsx
+++ src/Pages/APIs/index.tsx
@@ -4,7 +4,25 @@ import "./api.scss";
 const APIs = () => {
   return (
     <div>
-      <p>APIs</p>
+      <div id="navbar">
+        <h2>API’S</h2>
+        <br />
+        <ul id="lista">
+          <li id="despesas" className="item">
+            DESPESAS
+          </li>
+          <li id="estrutura" className="item">
+            ESTRUTURA
+          </li>
+          <li id="receitas" className="item">
+            RECEITAS
+          </li>
+          <li id="municipios" className="item">
+            MUNICIPIOS
+          </li>
+        </ul>
+      </div>
+      <div id="content"></div>
     </div>
   );
 };
