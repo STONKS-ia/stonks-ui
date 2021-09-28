0c5bc4432193ef87413a3caec5027f5c7c204eaa
Pedro Chaves <pedro.chaves@cittamobi.com.br>
2021-09-27 22:58:47 -0300

    Página de Contatos feita

--- src/Pages/DataSet/index.tsx
+++ src/Pages/DataSet/index.tsx
@@ -8,9 +8,27 @@ const DataSet = () => {
     <div id="about">
       <img id="imagem" src={aboutImg} alt="yes" />
       <div id="information">
-        <img id="background-wave" src={waveBG} alt="" />
         <h2>Contato</h2>
-        <ul>Telefone</ul>
+        <ul>
+          <li className="item-list">
+            <b>Endereço:</b> Prédio Sede 7º andar
+          </li>
+          <li className="item-list">
+            <b>Ouvidoria: </b>
+            <a href="http://www4.tce.sp.gov.br/ouvidoria">
+              www4.tce.sp.gov.br/ouvidoria
+            </a>
+          </li>
+          <li className="item-list">
+            <b>Central de Atendimento:</b> 0800 8007575
+          </li>
+          <li className="item-list">
+            <b>WhatsApp:</b> (11)99508.7638
+          </li>
+          <li className="item-list">
+            <b>Email:</b> ouvidoria@tce.sp.gov.br
+          </li>
+        </ul>
       </div>
     </div>
   );
