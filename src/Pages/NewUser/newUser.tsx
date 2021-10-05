2b990ce1ed0d50428bb79fc50b02d50252acbe14
jvmoraisjunior <jvmoraisjunior@hotmail.com>
2021-10-05 10:13:11 -0300

    User insert page :construction:

---
+++ src/Pages/NewUser/newUser.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./login.scss";
+import user from "../../assets/img/user.svg";
+import pass from "../../assets/img/password.svg";
+
+const newUser = () => {
+  return (
+    <div>
+      <div id="divNewUser">
+        <h3>Novo Usuário</h3>
+        <input type="text" id="txtUser"></input><br />
+        <input type="text" id="txtEmail"></input><br />
+        <input type="text" id="txtPass"></input><br />
+        <input type="text" id="txtPhone"></input><br />
+        <input type="text" id="txtLocation"></input><br />
+        <button type="submit" id="btnCadastrar">Cadastrar</button><br />
+      </div>
+    </div>
+  );
+};
+export default newUser;
