01c561819d9eff4e86e196d80f1f7efeae6f02e5
viniciu scarabelli <vinicius.scarabelli@cittamobi.com.br>
2021-10-05 15:54:22 -0300

    :contruction: beginning table

--- src/Components/Table/index.tsx
+++ src/Components/Table/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./table.scss";
 
-function Table(cityId: any , name: string, imgUrl: string, originalPortalUrl: string, year: number, month: number, type: string) {
-console.log(month)
+function Table(filter: any) {
+  const { cityId, name, imgUrl, originalPortalUrl, month, year, type } = filter;
   return (
     <div className="table">
     </div>
--- src/Pages/DetailCity/index.tsx
+++ src/Pages/DetailCity/index.tsx
@@ -28,42 +28,40 @@ const Detail = () => {
   const { id } = useParams<{id?: any}>();
   const [city, setCity] = useState<CityProps[]>([]);
   const [optionYear, setOptionYear] = useState<OptionProps[]>([]);
-  const [valueYear , setValueYear ] = useState();
-  const [valueMonth , setValueMonth ] = useState();
-  const [valueType , setValueType ] = useState();
-
-let optionTypes: OptionProps[] = [
-  {value: "receitas", label:"Receitas"},
-  {value: "despesas", label:"Despesas"},
-]
-let optionMonth: OptionProps[] = [
-  {value: 0, label:"January"},
-  {value: 1, label:"February"},
-  {value: 2, label:"March"},
-  {value: 3, label:"April"},
-  {value: 4, label:"May"},
-  {value: 5, label:"June"},
-  {value: 6, label:"July"},
-  {value: 7, label:"August"},
-  {value: 8, label:"September"},
-  {value: 9, label:"October"},
-  {value: 10, label:"November"},
-  {value: 11, label:"December"},
-]
-
-  function createYearOption() {
-    let resultArray: OptionProps[] = [];
-    for (let i = 0; i <= 20; i++) {
-        let year = thisYear - i;
-        let result = {value: year, label: year}
-        resultArray.push(result);
+  let optionTypes: OptionProps[] = [
+    {value: "receitas", label:"Receitas"},
+    {value: "despesas", label:"Despesas"},
+  ]
+  let optionMonth: OptionProps[] = [
+    {value: 0, label:"January"},
+    {value: 1, label:"February"},
+    {value: 2, label:"March"},
+    {value: 3, label:"April"},
+    {value: 4, label:"May"},
+    {value: 5, label:"June"},
+    {value: 6, label:"July"},
+    {value: 7, label:"August"},
+    {value: 8, label:"September"},
+    {value: 9, label:"October"},
+    {value: 10, label:"November"},
+    {value: 11, label:"December"},
+  ]
+    function createYearOption() {
+      let resultArray: OptionProps[] = [];
+      for (let i = 0; i <= 20; i++) {
+          let year = thisYear - i;
+          let result = {value: year, label: year}
+          resultArray.push(result);
+      }
+      setOptionYear(resultArray);
     }
-    setOptionYear(resultArray);
-  }
-  useEffect(() =>{
-    createYearOption();
-  }, [])
-
+    useEffect(() =>{
+      createYearOption();
+      console.log()
+    }, [])
+    const [valueYear , setValueYear ] = useState(thisYear);
+    const [valueMonth , setValueMonth ] = useState(thisMonth);
+    const [valueType , setValueType ] = useState("receitas");
   async function getCityById() {
     try{
       const res = await api.get(`/stonks/cities/${id}`);
@@ -79,8 +77,8 @@ let optionMonth: OptionProps[] = [
   useEffect(() =>{
     getCityById();
   }, [])
-  const cityDiv = city.map( (div, key) =>{
-    const {cityId, name, imgUrl, originalPortalUrl } = div;
+  const cityDiv = city.map( (cityDetails, key) =>{
+    const {cityId, name, imgUrl, originalPortalUrl } = cityDetails;
     return(
      <section className="city-container" key={key}>
       <p className="title">{name}</p>
