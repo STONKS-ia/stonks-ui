437e1239b0938ffda1b901a1116cbb3e3bb7b368
Pedro Chaves <pedro.chaves@cittamobi.com.br>
2021-10-08 19:00:17 -0300

    Tabela Parcialmente feita

--- src/Pages/List/index.tsx
+++ src/Pages/List/index.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { ToastContainer } from 'react-toastify';
-import success from '../../utils/success';
-import error from '../../utils/error';
-import 'react-toastify/dist/ReactToastify.css';
-import api from '../../services/api';
+import { ToastContainer } from "react-toastify";
+import success from "../../utils/success";
+import error from "../../utils/error";
+import "react-toastify/dist/ReactToastify.css";
+import api from "../../services/api";
 import listStyle from "./list.module.scss";
 
 type CityProps = {
@@ -18,35 +18,45 @@ const List = () => {
   const [input, setInput] = useState("");
 
   async function getCities() {
-    try{
+    try {
       const res = await api.get("/stonks/cities");
-      const { data:{result}  } = res;
+      const {
+        data: { result },
+      } = res;
       success("Municipio carregado");
       return setCities(result);
-    }catch(err){
+    } catch (err) {
       console.error(err);
       error("Municipio nao carregado");
       return [null, err];
     }
   }
-  const city = cities.filter(city => city.name.toLowerCase().includes(input.toLowerCase())).map((city, key) => {
-          const { cityId, name, imgUrl } = city;
-          const url = `city/${cityId}`
-          return (
-            <Link to={url} className="city" key={key}>
-                <img id="imgUrl" src={imgUrl} alt={name} />
-                <p id="name">{name}</p>
-            </Link>
-          )
-  })
-  useEffect(() =>{
+  const city = cities
+    .filter((city) => city.name.toLowerCase().includes(input.toLowerCase()))
+    .map((city, key) => {
+      const { cityId, name, imgUrl } = city;
+      const url = `city/${cityId}`;
+      return (
+        <Link to={url} className={listStyle.city} key={key}>
+          <img id={listStyle.imgUrl} src={imgUrl} alt={name} />
+          <p id={listStyle.name}>{name}</p>
+        </Link>
+      );
+    });
+  useEffect(() => {
     getCities();
   }, []);
   return (
     <main className={listStyle.listContainer}>
-    <ToastContainer />
-        <div className={listStyle.search}>
-        <input type="text" name="search" onChange={e => setInput(e.target.value)} value={input} placeholder="Search city by name"/>
+      <ToastContainer />
+      <div className={listStyle.search}>
+        <input
+          type="text"
+          name="search"
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+          placeholder="Search city by name"
+        />
         <div className={listStyle.icon}>
           <svg id={listStyle.icon} viewBox="0 0 100 100">
             <circle id={listStyle.pie} cx="45" cy="45" r="20"></circle>
@@ -56,9 +66,7 @@ const List = () => {
           </svg>
         </div>
       </div>
-      <div className={listStyle.result}>
-        { city }
-      </div>
+      <div className={listStyle.result}>{city}</div>
     </main>
   );
 };
