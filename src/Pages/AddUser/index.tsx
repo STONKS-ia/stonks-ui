e62b8141f23791922dc01348f84838048eb8a019
Pedro Chaves <pedro.chaves@cittamobi.com.br>
2021-10-11 01:21:11 -0300

    Botao de ROLE adicionado

--- src/Pages/AddUser/index.tsx
+++ src/Pages/AddUser/index.tsx
@@ -1,13 +1,26 @@
-import React, { useRef} from "react";
+import React, { useRef, useState} from "react";
 import {FormHandles } from '@unform/core'
 import { Form } from '@unform/web'
+import Select from 'react-select';
 
 import Input  from "../../components/Input"
 import newUserStyle from "./newUser.module.scss";
 import '../../assets/form.scss'
+import customTheme from "../../assets/theme";
+import customStyles from "../../assets/style";
+
+type OptionProps = {
+  value: any,
+  label: any
+}
 
 const AddUser = () => {
+  const [valueUser , setUserType ] = useState("receitas");
   const formRef = useRef<FormHandles>(null);
+  let optionUsers: OptionProps[] = [
+    {value: "user", label:"Usuário comum"},
+    {value: "admin", label:"Administrador"},
+  ]
 
   const handleFormSubmit = () =>{
 
@@ -21,6 +34,7 @@ const AddUser = () => {
         <Input name="password" type="password" className="inputField" id={newUserStyle.txtPass} />
         <Input name="phone" type="text" className="inputField" id={newUserStyle.txtPhone} />
         <Input name="cityUser" type="text" className="inputField" id={newUserStyle.txtLocation} />
+        <Select name="roleUser" id={newUserStyle.select} theme={customTheme} styles={customStyles} options={optionUsers} onChange={(e: any) => setUserType(e.value)} defaultValue={optionUsers[0]}  isSearchable={false}/>
         <button type="submit" className="btnEntrar" id="btnCadastrar">Cadastrar</button>
       </Form>
     </>
